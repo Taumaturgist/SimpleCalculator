4c7b23e1f8f9fee2bdbf841e10bfa9a7f78d1955
Taumaturgist <va.alekseevskiy@gmail.com>
2021-11-14 12:49:35 +0700

    Alekseevskiy homework

--- Calculator/CalculatorModel.cs
+++ Calculator/CalculatorModel.cs
@@ -12,10 +12,20 @@ namespace Calculator
         {
             
         }
-
+        //не понял про "для каждой операции создать отдельный метод"?
+        //вроде как все прекрасно реализуется внутри метода Calculate?
+        //через условную конструкцию...
+        //или задание существенно проще, чем мне показалось
+        //или я его конкретно не понял))
+        //но вроде считает и на ноль не делит
         public object Calculate(int operand1, int operand2, char operation)
         {
-            return null;
+            if (operation == '+') return operand1 + operand2;
+            else if (operation == '-') return operand1 - operand2;
+            else if (operation == '*') return operand1 * operand2;
+            else if (operation == '/' && operand2 != 0) return operand1 / operand2;
+            else if (operation == '/' && operand2 == 0) return null;
+            else return null;
         }
 
         
